35fd2aa1aa06a0bc93eb54f8209907d95209af45
Javier Neus <neusjavier@gmail.com>
2024-03-09 22:36:46 -0300

    Add Tailwind styles

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 export function Navigation() {
     return (
-        <div>
+        <div className="flex justify-between py-3">
             <Link to='tasks'>
-                <h1> Task App </h1>
+                <h1 className="font-bold text-3xl mb-4 text-black"> Listar tareas </h1>
             </Link>
-            <Link to='tasks_create'> create Task</Link>
+            <button className="bg-blue-500 px-3 py-2 rounded-lg">
+                <Link to='tasks_create'> Crear tareas</Link>
+            </button>
         </div>
     )
 }
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -2,6 +2,7 @@ import {useForm} from 'react-hook-form';
 import { createTask, deleteTask, getTaskById, updateTask } from '../api/Task.api';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
+import toast from 'react-hot-toast';
 
 
 export function TaskFormPage() {
@@ -30,36 +31,49 @@ export function TaskFormPage() {
     const onSubmit = handleSubmit(async data => {
         if (id_task) {
             await updateTask(id_task, data);
+            toast.success('Tarea Editada', {position: 'bottom-center', })
         } else {
             await createTask(data);
+            toast.success('Tarea creada', {position: 'bottom-center', })
         }
         navigate('/tasks');
     })
 
     return (
-        <div>
+        <div className='max-w-xl mx-auto'>
             <form onSubmit={onSubmit}>
-                <input type="text" placeholder="Título"
+                <input 
+                    type="text"
+                    placeholder="Título"
                     {...register('title', {required: true})}
+                    className='bg-zinc-900 p-3 rounded-lg block w-full mb-3'
                 />
                 {errors.title && <span>El título es requerido</span>}
-                <textarea rows="3" placeholder="Descripción"
+                <textarea
+                    rows="3" 
+                    placeholder="Descripción"
                     {...register('description', {required: true})}
+                    className='bg-zinc-900 p-3 rounded-lg block w-full mb-3'
                 ></textarea>
                 {errors.description && <span>La descripción es requerido</span>}
 
-                <button>Guardar</button>
+                <div className='flex justify-around'>
+                    <button className='bg-green-600 p-3 rounded-lg block w-48 mt-3'>Guardar</button>
+                    {id_task && <input type='button' value={'Borrar'}
+                        className='bg-red-600 p-3 rounded-lg w-48 mt-3'
+                        onClick={async () =>{
+                            const validate_delete = window.confirm('¿Está seguro de eliminar la tarea?')
+                            if (validate_delete) {
+                                await deleteTask(id_task);
+                                toast.success('Tarea Eliminada', {position: 'bottom-center', })
+                                navigate('/tasks');
+                            }
+                        }}/>
+                    }
+                </div>
             </form>
 
-            {id_task && <button 
-                onClick={async () =>{
-                    const validate_delete = window.confirm('¿Está seguro de eliminar la tarea?')
-                    if (validate_delete) {
-                        await deleteTask(id_task);
-                        navigate('/tasks');
-                    }
-                }}
-            >Borrar</button> }
+            
         </div>
     )
 }
